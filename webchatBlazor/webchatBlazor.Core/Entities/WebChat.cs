f1f630361fd3710c546aa97cda515dcb5c823393
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-27 17:59:39 -0300

    Criação da entidade WebChat e das pastas de repositórios

---
+++ webchatBlazor/webchatBlazor.Core/Entities/WebChat.cs
@@ -0,0 +1,28 @@
+﻿namespace webchatBlazor.Core.Entities
+{
+    public class WebChat
+    {
+        public int? IdChat { get; set; }
+        public string Pergunta { get; set; }
+        public string Resposta { get; set; }
+        public bool ArquivoEmAnexo { get; set; } = false;
+
+        public WebChat() { }
+
+
+        public WebChat(int idChat, string pergunta, string resposta)
+        {
+            IdChat = idChat;
+            Pergunta = pergunta;
+            Resposta = resposta;
+        }
+
+        public WebChat(int idChat, string pergunta, string resposta, bool anexo)
+        {
+            IdChat = idChat;
+            Pergunta = pergunta;
+            Resposta = resposta;
+            ArquivoEmAnexo = anexo;
+        }
+    }
+}
