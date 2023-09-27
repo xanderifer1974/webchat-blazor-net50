96e956a35ba39b5038e9cd5bcf55e42d14ddecc7
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-26 22:58:21 -0300

    Preparando o ambiente para o webChat usando o Blazor

---
+++ webchatBlazor/webchatBlazor.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace webchatBlazor.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ webchatBlazor/webchatBlazor.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace webchatBlazor.Core
+{
+    public class Class1
+    {
+    }
+}
