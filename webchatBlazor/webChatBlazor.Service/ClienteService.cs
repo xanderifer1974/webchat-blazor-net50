c73ce7fa4675c583258595014e85dc332f060993
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-29 16:29:55 -0300

    Criação do Serviço de Clientes.

---
+++ webchatBlazor/webChatBlazor.Service/ClienteService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using webchatBlazor.Business.Clientes.Interfaces;
+using webchatBlazor.Core.Entities;
+using webchatBlazor.Service.Interface;
+
+namespace webchatBlazor.Service
+{
+    public class ClienteService : IClienteService
+    {
+        private readonly IExibeClientePorCpf _exibeClientePorCpf;
+        private readonly IListaClientes _listaClientes;
+
+        public ClienteService(IExibeClientePorCpf exibeClientePorCpf, IListaClientes listaClientes)
+        {
+            _exibeClientePorCpf = exibeClientePorCpf;
+            _listaClientes = listaClientes;
+        }
+        public Cliente BuscarClientePorCPF(long cpf)
+        {
+            return _exibeClientePorCpf.BuscarClientePorCPF(cpf);
+        }
+
+        public IEnumerable<Cliente> ListarClientes(string filter = null)
+        {
+            return _listaClientes.ListarClientes(filter);
+        }
+    }
+}
