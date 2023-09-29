bb4960a69bd000aed8f3333989e165e4126052a3
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-28 21:41:28 -0300

    Criação da Entidade Cliente

---
+++ webchatBlazor/webchatBlazor.Core/Entities/Cliente.cs
@@ -0,0 +1,25 @@
+﻿namespace webchatBlazor.Core.Entities
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public long Cpf { get; set; }
+        public string NomeCompleto { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+
+        public Cliente(int idCliente, long cpf, string nomeCompleto, string nome, bool ativo)
+        {
+            IdCliente = idCliente;
+            Cpf = cpf;
+            NomeCompleto = nomeCompleto;
+            Nome = nome;
+            Ativo = ativo;
+        }
+
+        public Cliente()
+        {
+
+        }
+    }
+}
