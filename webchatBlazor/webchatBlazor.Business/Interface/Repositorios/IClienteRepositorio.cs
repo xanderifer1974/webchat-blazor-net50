ee563f7cfab0f4fca6b27740fb5eb4d8adb1eccf
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-28 21:50:15 -0300

    Criação de interface IClienteRepositorio

---
+++ webchatBlazor/webchatBlazor.Business/Interface/Repositorios/IClienteRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using webchatBlazor.Core.Entities;
+
+namespace webchatBlazor.Business.Interface.Repositorios
+{
+    public interface IClienteRepositorio
+    {
+        IEnumerable<Cliente> ListarClientes(string filter = null);
+        Cliente BuscarClientePorCPF(long cpf);
+    }
+}
