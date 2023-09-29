604f9979dd2a62806a15ebedd0859581a042e66b
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-29 10:10:05 -0300

    Conclusão do repositório de clientes

--- webchatBlazor/webchatBlazor.Data/Repository/ClienteRepositorio.cs
+++ webchatBlazor/webchatBlazor.Data/Repository/ClienteRepositorio.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using webchatBlazor.Business.Clientes;
 using webchatBlazor.Business.Interface.Repositorios;
 using webchatBlazor.Core.Entities;
 
