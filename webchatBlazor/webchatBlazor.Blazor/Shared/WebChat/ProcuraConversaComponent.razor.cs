5432504d4bc907af6eb6ffb6902bd85ef8640154
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-09-30 00:14:12 -0300

    Inicio da criação do componente Procurar Conversa Component

---
+++ webchatBlazor/webchatBlazor.Blazor/Shared/WebChat/ProcuraConversaComponent.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace webchatBlazor.Blazor.Shared.WebChat
+{
+    public partial class ProcuraConversaComponent
+    {
+        public string filter { get; set; }
+
+        public void  handerSearch()
+        {
+            
+        }
+    }
+}
