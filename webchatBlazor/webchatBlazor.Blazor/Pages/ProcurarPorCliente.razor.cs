182ac749064dde71f04b90da038ef3dbc87c7a54
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-10-02 15:10:07 -0300

    Criação Página Procurar Clientes

---
+++ webchatBlazor/webchatBlazor.Blazor/Pages/ProcurarPorCliente.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using webchatBlazor.Core.Entities;
+using webchatBlazor.Service.Interface;
+
+namespace webchatBlazor.Blazor.Pages
+{
+    public partial class ProcurarPorCliente
+    {
+        [Inject]
+        public IClienteService ClienteService { get; set; }
+
+        private IEnumerable<Cliente> Clientes { get; set;}
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Clientes = ClienteService.ListarClientes();
+        }
+
+        private void HandlerSearch(string filter)
+        {
+            Clientes = ClienteService.ListarClientes(filter);
+        }
+    }
+}
