35f1707fa4da79dd17978a8b3d575f1c91566f52
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-10-02 18:15:26 -0300

    Implementação das mensagens, mas sem formatação e horários

---
+++ webchatBlazor/webchatBlazor.Blazor/Shared/WebChatComponents/WebChatComponent.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using webchatBlazor.Core.Entities;
+using webchatBlazor.Service.Interface;
+
+namespace webchatBlazor.Blazor.Shared.WebChatComponents
+{
+    public partial class WebChatComponent
+    {
+        [Inject] 
+        public IWebChatService webChatService {get; set;}
+
+
+        [Parameter]
+        public string TituloChat { get; set; }
+
+       private List<string> Mensagens {get; set;}
+
+       public string mensagem = "";
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Mensagens = new List<string>();
+        }
+
+        private void SendMessage()
+        {
+            if (!string.IsNullOrWhiteSpace(mensagem))
+            {
+                Mensagens.Add($"Você: {mensagem}");
+               
+                WebChat chat = webChatService.BuscarConversaPorPergunta(mensagem);
+                Mensagens.Add($"API: {chat.Resposta}");
+
+                mensagem = ""; // Limpar a caixa de entrada após o envio
+            }
+        }
+    }
+}
