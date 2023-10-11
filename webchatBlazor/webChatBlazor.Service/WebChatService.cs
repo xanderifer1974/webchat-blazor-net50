853916425710432583d5e0044bbb13c810edadb9
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-10-10 22:51:07 -0300

    Ajuste de regra de negócio no projeto business

--- webchatBlazor/webChatBlazor.Service/WebChatService.cs
+++ webchatBlazor/webChatBlazor.Service/WebChatService.cs
@@ -11,13 +11,11 @@ namespace webchatBlazor.Service
     {
        
         private readonly IConversaManager _conversaManager;
-        private readonly IClienteManager _clienteManager;
+       
 
-        public WebChatService( IConversaManager conversaManager, IClienteManager cliente)
-        {       
-         
-            _conversaManager = conversaManager;
-            _clienteManager = cliente;
+        public WebChatService( IConversaManager conversaManager)
+        {                
+            _conversaManager = conversaManager;          
 
         }
 
@@ -33,45 +31,7 @@ namespace webchatBlazor.Service
 
         public WebChat BuscarConversaPorPergunta(string pergunta)
         {
-            WebChat webChat = new();
-            Cliente cliente = new();
-            string nome = string.Empty;
-            string resposta = string.Empty;
-
-            if (Validacao.ValidarCPF(pergunta))
-            {
-                long cpf = long.Parse(pergunta);
-                cliente = _clienteManager.BuscarClientePorCPF(cpf);
-
-                if (cliente != null)
-                {
-                    nome = cliente.Nome;
-
-                    resposta = $"Funny: <h6>Olá <b>{nome}</b>, escolha uma das opções abaixo.</h6>" +
-                        $"<ul><li>01 – Alterar pacote de canais</li>" +
-                        $"<li>02 – Alterar dados cadastrais</li>" +
-                        $"<li>03 – Solicitar um novo ponto</li>" +
-                        $"<li>04 – Alterar endereço da assinatura</li>" +
-                        $"<li>05 – Solicitar segunda via da fatura</li>" +
-                        $"<li>06 – Renegociar dívida</li>" +
-                        $"<li>07 – Cancelar assinatura</li>" +
-                        $"<li>08 - Falar com um atendente</li></ul>";
-
-                    webChat.Resposta = resposta;
-                }
-                else
-                {
-                    resposta = "Funny: Não encontramos seu CPF em nossas bases de cadastro. Favor informar um CPF válido.";
-                    webChat.Resposta = resposta;
-                }
-
-            }
-            else
-            {
-                webChat = _conversaManager.RealizaPergunta(pergunta);
-            }
-
-            return webChat;
+           return _conversaManager.RealizaPergunta(pergunta);
         }
 
         public IEnumerable<WebChat> BuscarConversas(string filter = null)
--- webchatBlazor/webchatBlazor.Business/Conversas/ConversaManager.cs
+++ webchatBlazor/webchatBlazor.Business/Conversas/ConversaManager.cs
@@ -2,16 +2,19 @@
 using webchatBlazor.Business.Conversas.Interfaces;
 using webchatBlazor.Business.Interface.Repositorios;
 using webchatBlazor.Core.Entities;
+using webchatBlazor.Util;
 
 namespace webchatBlazor.Business.Conversas
 {
     public class ConversaManager : IConversaManager
     {
         private readonly IWebChatRepositorio  _conversaRepositorio;
+        private readonly IClienteRepositorio _clienteRepositorio;
 
-        public ConversaManager(IWebChatRepositorio conversaRepositorio)
+        public ConversaManager(IWebChatRepositorio conversaRepositorio, IClienteRepositorio clienteRepositorio)
         {
             _conversaRepositorio = conversaRepositorio;
+            _clienteRepositorio = clienteRepositorio;
         }
         public bool AdicionarConversa(WebChat conversa)
         {
@@ -35,7 +38,47 @@ namespace webchatBlazor.Business.Conversas
 
         public WebChat RealizaPergunta(string pergunta)
         {
-            return _conversaRepositorio.BuscarConversaPorPergunta(pergunta);
-        }
+            WebChat webChat = new();
+            Cliente cliente = new();
+            string nome = string.Empty;
+            string resposta = string.Empty;
+
+            if (Validacao.ValidarCPF(pergunta))
+            {
+                long cpf = long.Parse(pergunta);
+                cliente = _clienteRepositorio.BuscarClientePorCPF(cpf);
+
+                if (cliente != null)
+                {
+                    nome = cliente.Nome;
+
+                    resposta = $"Funny: <h6>Olá <b>{nome}</b>, escolha uma das opções abaixo.</h6>" +
+                        $"<ul><li>01 – Alterar pacote de canais</li>" +
+                        $"<li>02 – Alterar dados cadastrais</li>" +
+                        $"<li>03 – Solicitar um novo ponto</li>" +
+                        $"<li>04 – Alterar endereço da assinatura</li>" +
+                        $"<li>05 – Solicitar segunda via da fatura</li>" +
+                        $"<li>06 – Renegociar dívida</li>" +
+                        $"<li>07 – Cancelar assinatura</li>" +
+                        $"<li>08 - Falar com um atendente</li></ul>";
+
+                    webChat.Resposta = resposta;
+                }
+                else
+                {
+                    resposta = "Funny: Não encontramos seu CPF em nossas bases de cadastro. Favor informar um CPF válido.";
+                    webChat.Resposta = resposta;
+                }
+
+            }
+            else
+            {
+                webChat = _conversaRepositorio.BuscarConversaPorPergunta(pergunta);
+            }
+
+            return webChat;
+        }   
+
+       
     }
 }
