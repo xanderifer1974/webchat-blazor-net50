1125a3faac1522d857cc01403d28565d889f2e8b
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-10-12 20:31:55 -0300

    Criação do formulário para incluir novo cliente

---
+++ webchatBlazor/webchatBlazor.Blazor/Pages/ClienteCrud/NovoCliente.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using webchatBlazor.Core.Entities;
+
+namespace webchatBlazor.Blazor.Pages.ClienteCrud
+{
+    public partial class NovoCliente
+    {
+        public Cliente Cliente { get; set; }       
+
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+       
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Cliente = new Cliente();
+            
+        }
+
+        protected void CriarCliente()
+        {
+            //Colocar a lógica de criação do cliente
+            // TODO >> Precisa fazer uma lógica para pegar o último id do, e na hora de adicionar na lista deve ser somado + 1
+            // Em relação ao Status, fazer uma combox que aparece Ativo e Inativo, mas que grave na classe true ou false
+        }
+
+        protected void Cancela()
+        {
+            NavigationManager.NavigateTo("/cliente");
+        }
+    }
+}
