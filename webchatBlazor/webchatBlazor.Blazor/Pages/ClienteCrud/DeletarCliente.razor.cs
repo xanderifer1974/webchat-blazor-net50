205576264b2279c3124a9fae89b661da8645e445
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2023-10-18 16:54:18 -0300

    Página para deletar cliente.

---
+++ webchatBlazor/webchatBlazor.Blazor/Pages/ClienteCrud/DeletarCliente.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using webchatBlazor.Core.Entities;
+using webchatBlazor.Core.Enuns;
+using webchatBlazor.Service.Interface;
+
+namespace webchatBlazor.Blazor.Pages.ClienteCrud
+{
+    public partial class DeletarCliente
+    {
+        [Inject]
+        public IClienteService ClienteService { get; set; }
+
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        [Parameter]
+        public int id { get; set; }
+
+        public Cliente Cliente { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Cliente = ClienteService.BuscarClientePorId(id);
+
+        }
+
+        protected void ExcluirCliente()
+        {
+            if (Cliente != null)
+            {
+                ClienteService.DeletarCliente(id);
+                Cliente.Alert.Mensagem = $"Cliente Id: {Cliente.IdCliente} -  Nome :{Cliente.NomeCompleto} excluido com sucesso!";
+                Cliente.Alert.Tipo = EnumAlert.Success;
+                Cliente.Alert.UrlRedirect = "/cliente";
+                Cliente.Alert.Titulo = "Cliente Excluído";
+            }
+            else
+            {
+                Cliente.Alert.Mensagem = $"Não foi possível excluir o cliente id: {id}!";
+                Cliente.Alert.Tipo = EnumAlert.Danger;
+                Cliente.Alert.UrlRedirect = "/cliente";
+                Cliente.Alert.Titulo = "Erro ao Excluir Cliente";
+            }
+
+            Cliente.Alert.ShowModal();
+            StateHasChanged();
+        }
+
+        protected void Cancela()
+        {
+            Navigation.NavigateTo("/cliente");
+        }
+
+
+    }
+}
--- webchatBlazor/webchatBlazor.Blazor/Pages/ConversaCrud/EditarConversa.razor.cs
+++ webchatBlazor/webchatBlazor.Blazor/Pages/ConversaCrud/EditarConversa.razor.cs
@@ -60,5 +60,7 @@ namespace webchatBlazor.Blazor.Pages.ConversaCrud
         {
             NavigationManager.NavigateTo("/conversa");
         }
+
+
     }
 }
